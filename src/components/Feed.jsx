f3daba1276a550a106da0571e34c12c32f7c1a13
hqvjet <viethq.1906@gmail.com>
2022-12-09 11:28:55 +0700

    update feed page logic

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,40 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Box, Stack, Typography} from "@mui/material";
 
-import { Videos, Sidebar } from "./index";
+import {Videos, Sidebar} from "./index";
 import {getSnippetFromAPI} from "../api/Youtube";
-import {videoCat} from "../utils/constants";
+import {categories, videoCat} from "../utils/constants";
+import Upload from "./UploadVideo/Upload";
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos, setVideos] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [videos, setVideos] = useState(null);
+    const [upload, setUpload] = useState(false);
+    const [search, setSearch] = useState(true);
+    const [storage, setStorage] = useState(false);
 
-  useEffect(() => {
-    setVideos(null);
+    useEffect(() => {
+        setVideos(null);
 
-    getSnippetFromAPI(videoCat.VIDEO, selectedCategory)
-      .then((data) => setVideos(data))
+        if (selectedCategory !== categories[0].name && selectedCategory !== categories[1].name) {
+            getSnippetFromAPI(videoCat.VIDEO, selectedCategory)
+                .then((data) => setVideos(data))
+            setSearch(true)
+            setStorage(false)
+            setUpload(false)
+        }
+
+        else if(selectedCategory === categories[0].name) {
+            setUpload(true)
+            setSearch(false)
+            setStorage(false)
+        }
+
+        else if(selectedCategory === categories[1].name) {
+            setStorage(true)
+            setUpload(false)
+            setSearch(false)
+        }
+
     }, [selectedCategory]);
 
-  return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
-        </Typography>
-      </Box>
+    return (
+        <Stack sx={{flexDirection: {sx: "column", md: "row"}}}>
+            <Box sx={{height: {sx: "auto", md: "92vh"}, borderRight: "1px solid #3d3d3d", px: {sx: 0, md: 2}}}>
+                <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
 
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
-        </Typography>
+                <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: "#fff",}}>
+                    Copyright © 2022 JSM Media
+                </Typography>
+            </Box>
 
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
-  );
+            <Box p={2} sx={{overflowY: "auto", height: "90vh", flex: 2}}>
+                <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: "white"}}>
+                    {selectedCategory} <span style={{color: "#FC1503"}}>videos</span>
+                </Typography>
+                {search && (
+                    <Videos videos={videos}/>
+                )}
+                {upload && (
+                    <Upload/>
+                )}
+                {storage && (
+                    <></>
+                )}
+            </Box>
+        </Stack>
+    );
 };
 
 export default Feed;
---
+++ src/components/UploadVideo/Upload.js
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+// import Textarea from '@mui/joy/Textarea';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark'
+    }
+});
+
+export default function Upload() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="title"
+                        name="title"
+                        label="Video Title"
+                        fullWidth
+                        autoComplete="off"
+                        variant="standard"
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <FormControl sx={{
+                        width: '250px'
+                    }}>
+                        <InputLabel id="hashtag-label">Category</InputLabel>
+                        <Select
+                            labelId="hashtag-label"
+                            id="hashtag"
+                            value={12}
+                            label="Category"
+                            // onChange={handleChange}
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+
+                <Grid item xs={12} sm={12}>
+                    {/*<Textarea*/}
+                    {/*    required*/}
+                    {/*    id="description"*/}
+                    {/*    name="description"*/}
+                    {/*    label="Video Description"*/}
+                    {/*    fullWidth*/}
+                    {/*    autoComplete="off"*/}
+                    {/*    variant="standard"*/}
+                    {/*/>*/}
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Button
+                        variant="contained"
+                        component="label"
+                    >
+                        Upload File
+                        <input
+                            type="file"
+                            hidden
+                        />
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControlLabel
+                        control={<Checkbox color="secondary" name="saveAddress" value="yes"/>}
+                        label="Use this address for payment details"
+                    />
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
